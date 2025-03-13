0e54e604799b99e5ca86086d6ccfe96ac704e666
Olalere1 <alabi.olalere@yahoo.com>
2025-03-13 20:28:42 +0000

    prod/stage-asg

---
+++ modules/prod-asg/variable.tf
@@ -0,0 +1,19 @@
+# This file contains the variables that are used in the prod-asg module.
+
+variable "pub-key" {}
+
+variable "nexus-ip-prd" {}
+
+variable "nr-key" {}
+variable "nr-acc-id" {}
+variable "nr-region" {}
+
+variable "asg-prd-name" {}
+
+variable "vpc-zone-identifier" {}
+
+variable "tg-prod" {}
+
+variable "redhat" {}
+
+variable "prod-sg" {}
